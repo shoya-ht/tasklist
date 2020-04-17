6a6652fa0cde6a6bb126904e60016ad0d0ee3310
shoya.ht <taro.kirameki@techacademy.jp>
2020-04-17 20:20:39 +0900

    final

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,64 @@
+package controllers;
+
+//新規登録
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+
+/**
+ * Servlet implementation class NewServlet
+ */
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        request.setAttribute("_token", request.getSession().getId());
+
+        request.setAttribute("task", new Task());
+        RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+
+        /*
+        EntityManager em=DBUtil.createEntityManager();
+        em.getTransaction().begin();
+
+        Message m= new Message();
+        String title="taro";
+        m.setTitle(title);
+        String content="hello";
+        m.setContent(content);
+
+        Timestamp currentTime=new Timestamp(System.currentTimeMillis());
+        m.setCreated_at(currentTime);
+        m.setUpdated_at(currentTime);
+
+        em.persist(m);
+        em.getTransaction().commit();
+
+
+        response.getWriter().append(Integer.valueOf(m.getId()).toString());
+   */
+    }
+
+}
